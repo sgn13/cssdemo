6f52304041808a75c1a7f9d871333f785ad832b4
Sagun Shrestha <sagun.shrestha@braintip.ai>
2024-11-14 04:22:04 +0545

    slkf

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-ReactDOM.render(app, appElement);
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,4 +9,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
