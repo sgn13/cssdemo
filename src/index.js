afb9d09b80e08fa900c0f95114f3d8f115f2d1cc
Sagun Shrestha <sagun.shrestha@braintip.ai>
2024-11-14 04:20:51 +0545

    soidfj

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
-const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-const appElement = document.getElementById("root");
-
 ReactDOM.render(app, appElement);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
